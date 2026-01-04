893d0f55f29af2fb6a1535ae33e26810d4cd184a
jenous11 <jenousdongol11@gmail.com>
2026-01-04 12:43:41 +0545

    resole an issue in delete

--- delete.php
+++ delete.php
@@ -4,8 +4,8 @@ require 'includes/Post.php';
 
 $id=$_GET['pid'];
     
-
+if(isset($_SESSION["id"]))
     $delete=new Post();
     $delete->deletepost($id);
     echo "The Post was Deleted!";
-    // header("Location: Index.php");
+    header("Location: logout.php");
