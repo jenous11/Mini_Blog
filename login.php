fc6224003182580c80f4ad126c67de919b5e5545
jenous11 <jenousdongol11@gmail.com>
2026-01-05 22:15:50 +0545

    added some tailwind  to the login

--- login.php
+++ login.php
@@ -5,21 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 
 <body>
-    <form action="auth.php" method="post">
+    <div class=" justify-self-center mt-22 bg-red-300 h-94 w-55 border-4 ">
+    <form action="auth.php" method="post" class="grid mt-10  justify-self-center">
         name:
-        <input type="text" name="name">
+        <input type="text" name="name" class="border-1 border-black rounded">
         <br><br>
         email:
-        <input type="email" name="email">
+        <input type="email" name="email" class="border-1 border-black rounded">
         <br><br>
         password:
-        <input type="password" name="password">
+        <input type="password" name="password" class="border-1 border-black rounded w-45">
         <br><br>
-        <button type="submit">submit</button>
+        <button class="rounded bg-sky-500 w-21" type="submit" >submit</button>
     </form>
+    </div>
+
 </body>
 
 </html>
