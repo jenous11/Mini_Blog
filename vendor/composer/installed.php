f45bd5c31592df431606ecc989a3b27108313311
jenous11 <jenousdongol11@gmail.com>
2026-01-30 18:09:01 +0545

    changed my name in composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jenous11/mini-blog-app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '28042e8bda7305847b722a675d4fae05086cad4c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jenous11/mini-blog-app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '28042e8bda7305847b722a675d4fae05086cad4c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
