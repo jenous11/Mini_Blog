34cc6fcb97b793aa9deb579917e311242567fb8a
jenous11 <jenousdongol11@gmail.com>
2026-02-28 20:09:47 +0545

    made the register logic better with same ui of login

--- auth/login.php
+++ auth/login.php
@@ -10,22 +10,22 @@
 <body >
         <!-- bootstrap -->
         <div class="d-flex  align-items-center  justify-content-center  bg-purple-200"  >
-        <form action="auth.php" method="post" style="margin-top: 5rem; width:450px; height:39rem;" class=" border border-dark rounded-5  bg-light " >
-          <label for="Name of the app" class="form-item mb-5 align-items-center d-flex justify-content-center "><h3>Mini-Blog-app</h3></label>
+        <form action="auth.php" method="post" style="margin-top: 5rem; width:450px; height:33rem;" class=" border border-dark rounded-5  bg-light mt-5 " >
+          <label for="Name of the app" class="form-item mb-5 align-items-center d-flex justify-content-center "><h4>Mini-Blog-app</h4></label>
         <div class=" justify-content-center  d-flex ">
-                <label class="form-label"> <h3> Name</label>
+                <label class="form-label"> <h4> Name</label>
                 <input type="text"  class="form-control mt-4  border border-dark " placeholder="Enter Name" style="width:20rem;" name="name" >
             </div>
             <div class=" justify-content-center d-flex">
-                <label class="form-label"  name="email"><h3>Email</label>
+                <label class="form-label"  name="email"><h4>Email</label>
                 <input type="email" class="form-control mt-4  border border-dark" style="width:20rem;"  placeholder="Enter Email"  name="email" required >
             </div>
             <div class=" justify-content-center d-flex">
-                <label class="form-label" name="password"><h3>Password</label>
+                <label class="form-label" name="password"><h4>Password</label>
                 <input type="password" class="form-control mt-2 mb-3 border border-dark" style="width:20rem;"  name="password"  placeholder="Enter Password"  required >
               </div>
-              <button type="submit" class="btn btn-primary mb-2" style="margin-left:2rem;"><h3>Submit</button>
-              <a href="register.php" class="mb-5"  style="margin-left:2rem;"><h3>register</a>
+              <button type="submit" class="btn btn-primary mb-2" style="margin-left:2rem;"><h4>Submit</button>
+              <a href="register.php" class="mb-2"  style="margin-left:2rem;"><h4>register</a>
         </form>
         </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
--- auth/register.php
+++ auth/register.php
@@ -11,23 +11,25 @@ session_start();
 </head>
 <body>
   <!-- <p style="color:green;">You must register first!!</p> -->
-  <div class="d-flex justify-content-center mt-5">
-    <form action="/Mini-Blog-app/views/createuser.php" method="post">
-      <div class="mb-3">
-        <label class="form-label" required>Name</label>
-        <input type="text" class="form-control" name="name">
-      </div>
-      <div class="mb-3">
-        <label class="form-label" name="email" required>Email address</label>
-        <input type="email" class="form-control" name="email">
-        <!-- <div>We'll never share your email with anyone else.</div> -->
-      </div>
-      <div class="mb-3">
-        <label class="form-label" name="password" required>Password</label>
-        <input type="password" class="form-control" name="password">
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+  <div class="d-flex  align-items-center  justify-content-center  bg-purple-200"  >
+    <form action="/Mini-Blog-app/views/createuser.php" method="post" style="margin-top: 5rem; width:450px; height:33rem;" class=" border border-dark rounded-5  bg-light mt-5 " >
+     <label for="Name of the app" class="form-item mb-5 align-items-center d-flex justify-content-center "><h4>Mini-Blog-app</h4></label>
+    <div class=" justify-content-center  d-flex ">
+                <label class="form-label"> <h4> Name</label>
+                <input type="text"  class="form-control mt-4  border border-dark " placeholder="Enter Name" style="width:20rem;" name="name" >
+            </div>
+            <div class=" justify-content-center d-flex">
+                <label class="form-label"  name="email"><h4>Email</label>
+                <input type="email" class="form-control mt-4  border border-dark" style="width:20rem;"  placeholder="Enter Email"  name="email" required >
+            </div>
+            <div class=" justify-content-center d-flex">
+                <label class="form-label" name="password"><h4>Password</label>
+                <input type="password" class="form-control mt-2 mb-3 border border-dark" style="width:20rem;"  name="password"  placeholder="Enter Password"  required >
+              </div>
+              <button type="submit" class="btn btn-primary  bg-purple  mb-2" style="margin-left:2rem;"><h4>Submit</button>
+              <a href="register.php" class="mb-2"  style="margin-left:2rem;"><h4>register</a>
+        </form>
+        </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   </div>
 </body>
